dc235d729ae10534f551e6fb031f13476ac3c8da
LorsK <lors.kushtov@mail.utoronto.ca>
2018-03-15 11:52:34 -0400

    Update getRideList.php

--- getRideList.php
+++ getRideList.php
@@ -17,7 +17,14 @@
 
 	$query_result = $mysql_connection->query("SELECT * FROM Rides $filter");
 	
-	while($row = mysqli_fetch_array($query_result)){
+	$rows = array();
+
+	while($row[0] = mysqli_fetch_array($query_result)){
+		$get_driver_query_result = $mysql_connection->query("
+			SELECT * FROM Users
+			WHERE Users.UserId = '".$row[0]["OwnerUserId"]."'
+			LIMIT 1");
+		$row[1] = $get_driver_query_result
 		$rows[] = $row;
 	}
  
