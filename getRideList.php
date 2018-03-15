3dde5f794e2e5e5de2c66f9e3903937c7d7f5326
LorsK <lors.kushtov@mail.utoronto.ca>
2018-03-15 11:38:22 -0400

    Update getRideList.php

--- getRideList.php
+++ getRideList.php
@@ -1,7 +1,21 @@
 <?php
 	require "connectionRequest.php";
+	$filter = '';
+
+	if (isset($_GET['PickupCity'])) {
+		$filter = 'WHERE PickupCity=\'' . htmlspecialchars($_GET['PickupCity']) . '\'';
+	}	
 	
-	$query_result = $mysql_connection->query("SELECT * FROM Rides");
+	if (isset($_GET['DropOffCity'])) {
+		if ($filter != ';) {
+			$filter .= " AND ";
+		} else {
+			$filter .= "WHERE";
+		}
+		$filter = "DropOffCity='" . htmlspecialchars($_GET['DropOffCity']) . "'";
+	}	
+
+	$query_result = $mysql_connection->query("SELECT * FROM Rides $filter");
 	
 	while($row = mysqli_fetch_array($query_result)){
 		$rows[] = $row;
